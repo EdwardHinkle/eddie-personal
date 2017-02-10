27cfc6f797ae0c2a09af15f42a78ab44246529ca
Edward Hinkle <eddie@meetmaestro.com>
2017-02-10 10:37:56 -0500

    Add _scripts for fetching external data for site

---
+++ _scripts/index.js
@@ -0,0 +1,15 @@
+var goodreads = require('goodreads');
+var gr = new goodreads.client({ key: 'KSXzo3GFar32jnjtiVlQjw', secret: 'hP7UPqEHVGUlDEdAd5JiRsiCwBj5OMDqPwe5rZz9o' });
+var fs = require("fs");
+
+gr.getSingleShelf({
+    userID: 22018436,
+    shelf: 'currently-reading',
+    page: 1,
+    per_page: 100
+}, function(currentlyReading){
+    var json = JSON.stringify(currentlyReading.GoodreadsResponse.books[0].book);
+    fs.writeFile('../_source/_data/currently-reading.json', json, 'utf8', function(){
+        console.log("Done");
+    });
+});
