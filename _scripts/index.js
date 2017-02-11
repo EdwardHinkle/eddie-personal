81224f42dea0e0f836bd318d6ca50b21d66b0773
Edward Hinkle <eddie@meetmaestro.com>
2017-02-11 01:05:32 -0500

    Add correct file paths and error handling

--- _scripts/index.js
+++ _scripts/index.js
@@ -9,8 +9,27 @@ gr.getSingleShelf({
     per_page: 100
 }, function(currentlyReading){
     var json = JSON.stringify(currentlyReading.GoodreadsResponse.books[0].book);
-    fs.writeFile('../_source/_data/currently_reading.json', json, 'utf8', function(){
-        console.log("Done");
+    fs.writeFile(__dirname + '/../_source/_data/currently_reading.json', json, 'utf8', function(error){
+        if (error != undefined) {
+            console.error("OOPS!", error);
+        }
+        console.log("Currently Reading List Retrieved");
+    });
+});
+
+gr.getSingleShelf({
+    userID: 22018436,
+    shelf: 'read',
+    page: 1,
+    per_page: 4,
+    sort: 'date_read'
+}, function(recentlyRead){
+    var json = JSON.stringify(recentlyRead.GoodreadsResponse.books[0].book);
+    fs.writeFile(__dirname + '/../_source/_data/recently_read.json', json, 'utf8', function(error){
+        if (error != undefined) {
+            console.error("OOPS!", error);
+        }
+        console.log("Recently Read List Retrieved");
     });
 });
 
